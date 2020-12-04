93a6fb3fe070683943bba9aee7fb9135200ded40
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-03 23:54:07 -0500

    Version 1.0.0

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -11,13 +12,58 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/2152b3f87b"; //-------- Hide this code on gitHub
+
+  const options = {
+    method: "POST",
+    auth: "rishi:91a9423ff83d0eb0af01e5c6a90a91eb-us7", //------- API key, hide this in gitHub
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, function () {
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+//Dynamic port declaration to work for Heroku and local system.
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
+
+//api Key
+//91a9423ff83d0eb0af01e5c6a90a91eb-us7
+
+// list ID
+// 2152b3f87b
